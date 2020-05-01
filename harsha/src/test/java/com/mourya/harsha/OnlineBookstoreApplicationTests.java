a637c4774d64d31e6067237c993ca8ff4c986141
Mourya <mourya1429@gmail.com>
2020-05-01 19:56:27 -0400

    Initial commit - created spring boot app and configured application file.

---
+++ harsha/src/test/java/com/mourya/harsha/OnlineBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mourya.harsha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
