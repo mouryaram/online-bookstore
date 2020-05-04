11926518532e53f4757d433d1cf3287dc3953052
mourya <mourya1429@gmail.com>
2020-05-04 19:50:34 -0400

    Created Angular Project and completed feature 1.

--- harsha/src/main/java/com/mourya/harsha/repository/BookRepository.java
+++ harsha/src/main/java/com/mourya/harsha/repository/BookRepository.java
@@ -1,11 +1,11 @@
 package com.mourya.harsha.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.mourya.harsha.entity.Book;
 
-
+@CrossOrigin("http://localhost:4201")
 public interface BookRepository extends JpaRepository<Book,Long> {
 
 }
